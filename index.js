6a483d3dcbe86f72018e833e155de7caf1506ab2
Akley <akleyalvescr@gmail.com>
2024-01-26 16:22:46 -0300

    scroll top no formulario de eventos

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 function copiarTexto() {
+    // Seleciona o elemento de texto
+    const elementoTexto = document.getElementById("texto");
+
     const textoParaCopiar = document.getElementById("texto");
     const textoSelecionado = window.getSelection();
     const range = document.createRange();
     range.selectNodeContents(textoParaCopiar);
     textoSelecionado.removeAllRanges();
     textoSelecionado.addRange(range);
-    document.execCommand("copy");
+
+
+    //copia o texto para a área de transferência
+    document.execCommand('copy');
+
+    // Rola a página para o topo
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
 }
